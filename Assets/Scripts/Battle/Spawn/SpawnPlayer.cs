191027644d99d815bdb36990f27cb42c1ad94f1a
Max <khryshcheniukmaksym@gmail.com>
2022-08-13 20:28:54 +0300

    Unity 2021.3.8f1 + фікс багу

--- Assets/Scripts/Battle/Spawn/SpawnPlayer.cs
+++ Assets/Scripts/Battle/Spawn/SpawnPlayer.cs
@@ -10,43 +10,14 @@ namespace CombatMechanics
     public class SpawnPlayer : SpawnPrefab
     {
         [SerializeField]
-        private Canvas _playingCanvas;
-        public Canvas PlayingCanvas
-        {
-            get => _playingCanvas;
-        }
-        [SerializeField]
-        private GameObject _moveButtonLeftPrefab;
-        public GameObject MoveButtonLeftPrefab
-        {
-            get => _moveButtonLeftPrefab;
-            set
-            {
-                _moveButtonLeftPrefab = value;
-            }
-        }
-        [SerializeField]
-        private GameObject _moveButtonRightPrefab;
-        public GameObject MoveButtonRightPrefab
-        {
-            get => _moveButtonRightPrefab;
-            set
-            {
-                _moveButtonRightPrefab = value;
-            }
-        }
         private GameObject LeftButton;
+        [SerializeField]
         private GameObject RightButton;
+
         private void Start()
         {
-            LeftButton = Instantiate(MoveButtonLeftPrefab);
-            RightButton = Instantiate(MoveButtonRightPrefab);
-
-            LeftButton.transform.SetParent(PlayingCanvas.transform);
-            RightButton.transform.SetParent(PlayingCanvas.transform);
-
-            LeftButton.transform.localPosition = MoveButtonLeftPrefab.transform.localPosition;
-            RightButton.transform.localPosition = MoveButtonRightPrefab.transform.localPosition;
+            LeftButton.SetActive(true);
+            RightButton.SetActive(true);
         }
     }
 }
