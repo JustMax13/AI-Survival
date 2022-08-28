9626e955e049f3c4e8a674abe8aa1e527ae11abf
Max <khryshcheniukmaksym@gmail.com>
2022-08-28 23:00:59 +0300

    Оновлення
    Додано виділення кнопки саме під час кліку, а не дотику до неї.

--- Assets/Scripts/EditorInGame/Canvas/ButtonSelected.cs
+++ Assets/Scripts/EditorInGame/Canvas/ButtonSelected.cs
@@ -2,15 +2,21 @@ using Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 namespace Editor
 {
-    public class ButtonSelected : MonoBehaviour
+    public class ButtonSelected : Button
     {
-        // 1) сделать так, чтобы деталь выбиралась только на клик
+        // | DONE |1) сделать так, чтобы деталь выбиралась только на клик 
         // 2) Когда деталь выбрана на ней появится Event Trigger 
         // 3) Кода выбраная деталь перестает быть выделенной - снимаем Event Trigger
-
+        public override void OnPointerDown(PointerEventData eventData) { }
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            base.OnPointerClick(eventData);
+            Select();
+        }
     }
 }
