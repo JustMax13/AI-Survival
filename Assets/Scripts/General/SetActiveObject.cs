f472f8b42700622d0406624ab1d7659004ccffe4
Max <khryshcheniukmaksym@gmail.com>
2022-09-13 14:35:27 +0300

    0.0.0.7.3
    Додано:
     1) Вимикання ігрового інтерфейсу під час паузи та вмикання його назад при виході з паузи

---
+++ Assets/Scripts/General/SetActiveObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveObject : MonoBehaviour
+{
+    public void ObjectON(GameObject obj) => obj.SetActive(true);
+
+    public void ObjectOFF(GameObject obj) => obj.SetActive(false);
+}
