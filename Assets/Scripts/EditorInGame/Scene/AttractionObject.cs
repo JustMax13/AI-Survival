fb0ac5e0960e0e979d39a6d7e653387961e30ce3
Max <khryshcheniukmaksym@gmail.com>
2022-09-13 21:47:48 +0300

    0.0.0.8.1
    Фікс багів:
     1) Тепер при пересування рам не буде "хибних"  ривків і пересувань рам, які стоять.

--- Assets/Scripts/EditorInGame/Scene/AttractionObject.cs
+++ Assets/Scripts/EditorInGame/Scene/AttractionObject.cs
@@ -7,14 +7,31 @@ namespace Editor
     public class AttractionObject : MonoBehaviour
     {
         public bool WasMouseDown { get; set; }
+        private const float _timeWasMouseDown = 0.2f;
+        private float _currentTimeWasMouseDown;
         public bool IsDrag { get; set; }
         private void Start()
         {
+            WasMouseDown = false;
+            IsDrag = false;
+            _currentTimeWasMouseDown = 0;
+        }
+        private void OnMouseDown() 
+        {
             WasMouseDown = true;
             IsDrag = true;
+        }  // как сделать так, что бы оно потом выключалось?
+        private void OnMouseUp()
+        {
+            IsDrag = false;
+            _currentTimeWasMouseDown = _timeWasMouseDown;
+        } 
+        private void Update()
+        {
+            if (_currentTimeWasMouseDown > 0)
+                _currentTimeWasMouseDown -= Time.deltaTime;
+            else if (WasMouseDown && !IsDrag)
+                WasMouseDown = false;
         }
-        private void OnMouseDown() => WasMouseDown = true;
-        private void OnMouseDrag() => IsDrag = true;
-        private void OnMouseUp() => IsDrag = false;
     }
 }
