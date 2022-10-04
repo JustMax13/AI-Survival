5f08b81dda50a4d400960d6d8101765c78d8816b
Max <khryshcheniukmaksym@gmail.com>
2022-10-04 19:13:00 +0300

    0.0.0.10.4.2
    Змінено:
     1) Платформу на андроид ( бо вона злетіла )

--- Assets/Scripts/Battle/Canvas/ConnectedShotButton.cs
+++ Assets/Scripts/Battle/Canvas/ConnectedShotButton.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 namespace CombatMechanics
 {
