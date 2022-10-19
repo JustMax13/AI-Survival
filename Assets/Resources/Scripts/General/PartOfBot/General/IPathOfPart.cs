75820ec33a90d12e1dd47924d36bdc59098bad99
Max <khryshcheniukmaksym@gmail.com>
2022-10-19 17:14:40 +0300

    0.0.0.10.5.2
    Перейменовано:
     1) Папки та пару скриптів

---
+++ Assets/Resources/Scripts/General/PartOfBot/General/IPathOfPart.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General.PartOfBots
+{
+    public interface IPathOfPart
+    {
+        public string Path { get; }
+    }
+}
---
+++ Assets/Resources/Scripts/General/PartOfBot/PathVariability/CentralBlock/ClassicCentralBlockPath.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General.PartOfBots
+{
+    public class ClassicCentralBlockPath : MonoBehaviour, IPathOfPart
+    {
+        public string Path => "Prefab/PartOfBot/Frame/ClassicCebtralBlock";
+    }
+}
